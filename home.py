b4a881bbff291cc6ac97dac924ff3274a0b4aaef
SuperZekes <markandsheenaknowles@gmail.com>
2024-03-25 12:28:43 -0400

    first commit

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config("Coke vs Pepsi", page_icon="🥤", layout="wide")
+
+# Generate random data for demonstration
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["This is not true info!", "Coke", "Pepsi"])
+
+st.line_chart(
+   chart_data, x="This is not true info!", y=["Coke", "Pepsi"], color=["#FF0000", "#0000FF"]
+)
